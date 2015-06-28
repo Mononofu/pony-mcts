f1b5ddaf55cfa96244a279dbbcd577de556757fa
Julian Schrittwieser <j.schrittwieser@gmail.com>
2015-06-28 19:47:43 +0100

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # MCTS for Go in PonyLang
 
 v 0.1: full implementation of the Go rules, including ko detection. Supports complete random playouts.
+
+# MCTS for Go in Rust
+
+- 2015-06-28: 150 ms for a full playout on a 19x19 board
--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,19 @@ extern crate time;
 mod go;
 
 fn main() {
+  for _ in 0 .. 500 {
+    play();
+  }
+}
+
+fn play() {
   let mut game = go::GoGame::new(19);
   let mut rng = rand::StdRng::from_seed(&[42]);
   let mut color_to_play = go::Stone::Black;
   let mut possible_moves = game.possible_moves(color_to_play);
   let mut num_consecutive_passes = 0;
   let mut num_moves = 0;
-  let start = time::PreciseTime::now();
+  // let start = time::PreciseTime::now();
 
   while num_consecutive_passes < 2 {
     num_moves += 1;
@@ -20,16 +26,17 @@ fn main() {
       Some(m) => {
         num_consecutive_passes = 0;
         game.play(color_to_play, *m, false);
-        println!("{}", game);
+        // println!("{}", game);
       }
       None => {
         num_consecutive_passes += 1;
-        println!("{:?} passes", color_to_play);
+        // println!("{:?} passes", color_to_play);
       }
     }
     color_to_play = color_to_play.opponent();
     possible_moves = game.possible_moves(color_to_play);
     // std::thread::sleep_ms(100);
   }
-  println!("{} moves in {}", num_moves, start.to(time::PreciseTime::now()));
+  // println!("{} moves in {}", num_moves, start.to(time::PreciseTime::now()));
+  // println!("{}", game);
 }
